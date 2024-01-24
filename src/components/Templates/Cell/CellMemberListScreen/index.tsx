7be610313a48f90855e98be73c4f787003ed0eb3
Covenant Nomad <covenantnomad@Covenantui-Macmini.local>
2024-01-25 00:18:22 +0900

    fix:@셀원정보, 셀원리스트 셀보고서 포함/미포함 분리정렬

--- src/components/Templates/Cell/CellMemberListScreen/index.tsx
+++ src/components/Templates/Cell/CellMemberListScreen/index.tsx
@@ -33,24 +33,62 @@ const CellMemberListScreen = ({}: CellMemberListScreenProps) => {
         <div className="w-full py-20 flex items-center justify-center">
           <Spinner />
         </div>
-      ) : data ? (
-        <div className="grid grid-cols-1 gap-y-3 lg:grid-cols-2 lg:gap-x-6 xl:grid-cols-3 xl:gap-x-8">
-          {data.myCellMembers
-            ?.filter((member) => !member.roles.includes(RoleType.CellLeader))
-            .sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0))
-            .map((member) => (
-              <CellMemberListItem
-                key={member.id}
-                userId={member.id}
-                name={member.name}
-                gender={member.gender!}
-                birthday={member.birthday || '1990-01-01'}
-                phone={member.phone}
-              />
-            ))}
-        </div>
       ) : (
-        <EmptyStateSimple />
+        <>
+          {data && data.myCellMembers ? (
+            <>
+              <div className="pt-2">
+                <h3 className="text-base font-medium leading-7 text-gray-900">
+                  셀보고서 작성 셀원
+                </h3>
+                <div className="grid grid-cols-1 gap-y-3 lg:grid-cols-2 lg:gap-x-6 xl:grid-cols-3 xl:gap-x-8">
+                  {data.myCellMembers
+                    .filter(
+                      (member) => !member.roles.includes(RoleType.CellLeader)
+                    )
+                    .filter((member) => member.isActive)
+                    .sort((a, b) => a.name.localeCompare(b.name))
+                    .map((member) => (
+                      <CellMemberListItem
+                        key={member.id}
+                        userId={member.id}
+                        name={member.name}
+                        gender={member.gender!}
+                        birthday={member.birthday || '1990-01-01'}
+                        phone={member.phone}
+                      />
+                    ))}
+                </div>
+              </div>
+              <Spacer size={'h-3'} />
+              <div className="pt-2">
+                <h3 className="text-base font-medium leading-7 text-gray-900">
+                  셀보고서 미작성 셀원
+                </h3>
+                <div className="grid grid-cols-1 gap-y-3 lg:grid-cols-2 lg:gap-x-6 xl:grid-cols-3 xl:gap-x-8">
+                  {data.myCellMembers
+                    .filter(
+                      (member) => !member.roles.includes(RoleType.CellLeader)
+                    )
+                    .filter((member) => !member.isActive)
+                    .sort((a, b) => a.name.localeCompare(b.name))
+                    .map((member) => (
+                      <CellMemberListItem
+                        key={member.id}
+                        userId={member.id}
+                        name={member.name}
+                        gender={member.gender!}
+                        birthday={member.birthday || '1990-01-01'}
+                        phone={member.phone}
+                      />
+                    ))}
+                </div>
+              </div>
+            </>
+          ) : (
+            <EmptyStateSimple />
+          )}
+        </>
       )}
       <Spacer size={'h-[50px]'} />
     </>
